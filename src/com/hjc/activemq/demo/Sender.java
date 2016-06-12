790e7e3a0cae55dc6ae2da514995a966724de36f
SSMTest <lingshao2010@126.com>
2016-06-12 18:50:18 +0800

    提交

--- src/com/hjc/activemq/demo/Sender.java
+++ src/com/hjc/activemq/demo/Sender.java
@@ -13,7 +13,7 @@ public class Sender {
 	private static final int SEND_NUMBER = 5;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
+		// TODO Auto-generated method stub
 		ConnectionFactory connectionFactory;
 		Connection connection = null;
 		Session session;
